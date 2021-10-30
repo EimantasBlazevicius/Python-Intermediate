10c78c0dee5ad52f84df24aadb52e921c15d032b
EimantasBlazevicius <eima.blaz@gmail.com>
2021-10-30 11:43:40 +0300

    star wars task text

--- start_wars_task.py
+++ start_wars_task.py
@@ -1,3 +1,11 @@
+"""
+Client request: convert their JSON to CSV.
+Client said nothing else and sales are not in the mood to ask questions,
+so do it the best way you imagine, so it would be easy to work with that
+CSV for who ever will receive it in the client side.
+"""
+
+
 import requests
 
 r = requests.get('https://swapi.dev/api/people')
