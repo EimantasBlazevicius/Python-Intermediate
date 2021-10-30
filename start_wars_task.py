3f6bce33224353cd1dfa3574f113159878816d56
EimantasBlazevicius <eima.blaz@gmail.com>
2021-10-30 12:54:27 +0300

    star wars update

--- start_wars_task.py
+++ start_wars_task.py
@@ -5,9 +5,30 @@ so do it the best way you imagine, so it would be easy to work with that
 CSV for who ever will receive it in the client side.
 """
 
-
 import requests
+import csv
+from time import sleep
 
-r = requests.get('https://swapi.dev/api/people')
 
-print(r.json())
+header = 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
+r = requests.get('https://swapi.dev/api/people', headers={'User-Agent': header})
+
+
+with open("star_wars.csv", "a+", newline='') as file:
+
+    writer = csv.writer(file)
+    data = r.json()
+    results = data.get('results')
+    writer.writerow(results[0].keys())
+    while True:
+        sleep(0.1)
+        data = r.json()
+        next_link = data.get('next')
+        results = data.get('results')
+        for result in results:
+            writer.writerow(result.values())
+
+        if next_link is not None:
+            r = requests.get(next_link, headers={'User-Agent': header})
+        else:
+            break
